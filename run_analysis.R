4147121553b9e08f655cbf5e7d74be6692d018f6
danielthornton <dpthorn@liverpool.ac.uk>
2018-04-21 10:14:46 +0100

    made a mistake when merging the data

--- run_analysis.R
+++ run_analysis.R
@@ -4,6 +4,10 @@
 # Summary: data set transformation of accelerometer and gyroscopic data    #
 ############################################################################
 
+library(data.table)
+install.packages("dplyr")
+library(dplyr)
+
 # STEP 1
 # Download and set working directory
 download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip","samsung_data.zip")
@@ -22,39 +26,41 @@ colnames(train_data) <- features$V2
 colnames(test_data) <- features$V2
 
 # STEP 4
+# Filter data to take only the standard deviation and mean columns
+test_data <- test_data[, grep(c("mean\\(|std\\("), names(test_data))]
+train_data <- train_data[, grep(c("mean\\(|std\\("), names(train_data))]
+
+
+# STEP 5
 # Obtain subject ids associated with each variable in the data sets
 subject_train <- read.table("train/subject_train.txt")
 subject_test <- read.table("test/subject_test.txt")
 colnames(subject_train) <- "subject_id"
 colnames(subject_test) <- "subject_id"
 
-# STEP 5
-# Obtain activity names for all variables, derived from the activity id
-activity_train <- read.table("train/y_train.txt")
-activity_test <- read.table("test/y_test.txt")
-activity_labels <- read.table("activity_labels.txt")
-activity_train <- merge(activity_train, activity_labels, by.x="V1",b.y="V1",all=TRUE)
-activity_test <- merge(activity_test, activity_labels, by.x="V1",b.y="V1",all=TRUE)
-colnames(activity_train) <- c("activity_id","activity_name")
-colnames(activity_test) <- c("activity_id","activity_name")
-
 # STEP 6
-# Merge the dataframes containing subject and activity information
-train_data_with_subject_and_activity <- cbind(subject_train, activity=activity_train[,"activity_name"], train_data)
-test_data_with_subject_and_activity <- cbind(subject_test, activity=activity_test[,"activity_name"], test_data)
+# Obtain activity ids for all variables, add to the data sets
+activity_y_train <- read.table("train/y_train.txt")
+activity_y_test <- read.table("test/y_test.txt")
+train_data_with_activity <- cbind(subject_train, activity=activity_y_train$V1, train_data)
+test_data_with_activity <- cbind(subject_test, activity=activity_y_test$V1, test_data)
 
-# STEP 7
-# Filter data to take only the standard deviation and mean columns
-filtered_test_data <- test_data_with_subject_and_activity[, grep(c("mean\\(|std\\(|subject_id|activity"), names(test_data_with_subject_and_activity))]
-filtered_train_data <- train_data_with_subject_and_activity[, grep(c("mean\\(|std\\(|subject_id|activity"), names(train_data_with_subject_and_activity))]
+## STEP 7
+# Create one dataset by merging test and train sets
+all_data <- rbind(train_data_with_activity,test_data_with_activity)
 
-# STEP 8
-# Merge test and training datasets
-test_and_train_rows <- rbind(filtered_test_data, filtered_train_data)
+## STEP 8
+# Label the activities using the activity names instead of the ids
+activity_labels <- read.table("activity_labels.txt")
+all_data <- merge(all_data, activity_labels, by.x="activity",by.y="V1")
+all_data <- subset(all_data, select = -c(activity))
+setnames(all_data, "V2", "activity_name")
+all_data<-all_data[,c(68, 1:(ncol(all_data)-1))]
+str(all_data)
 
 # STEP 9
 # Obtain a summary dataframe comprising the means of all variables in STEP 7 
 # grouped by subject and activity
-test_and_train_row_mean <- test_and_train_rows %>% group_by(subject_id, activity) %>%summarise_all(funs(mean))
-test_and_train_row_mean <- as.data.frame(test_and_train_row_mean)
-write.table(test_and_train_row_mean, file="tidy_dataset.txt", row.name=FALSE)
+all_data_means <- all_data %>% group_by(subject_id, activity_name) %>%summarise_all(funs(mean))
+all_data_means_df <- as.data.frame(all_data_means)
+write.table(all_data_means_df, file="tidy_dataset.txt", row.name=FALSE)
